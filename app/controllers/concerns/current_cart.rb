cf953c2f2ae826be31438936c20267f34f38d186
vicval <vicente.franca.valcarcel@gmail.com>
2017-07-27 14:29:33 -0300

    resposta do exercicio do final do capitulo D

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,9 +3,12 @@ module CurrentCart
 	private
 
 		def set_cart
-			@cart = Cart.find(session[:cart_id])
+			@cart = Cart.find(session[:cart_id])			
+			session[:counter] = 0
 		rescue ActiveRecord::RecordNotFound
 			@cart = Cart.create
 			session[:cart_id] = @cart.id
+			
+
 		end
 	end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,17 @@
 class StoreController < ApplicationController
-  def index
-  	@products = Product.order(:title)
-  end
+	include CurrentCart
+
+	 def index
+	 	@products = Product.order(:title)
+	 	contador	 	
+	 end
+
+	 def contador
+	 	if session[:counter].nil?
+	 		session[:counter]=0
+	 	else
+	 		session[:counter]+=1
+	 	end
+	 	puts session[:counter]
+	 end
 end
