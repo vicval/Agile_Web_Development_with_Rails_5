873ae12f4388ac2473b3ec58feaa723616ef362d
vicval <vicente.franca.valcarcel@gmail.com>
2017-07-27 11:53:22 -0300

    D3

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -51,4 +51,20 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count',0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
+  test "should destroy prodct" do
+    assert_difference('Product.count', -1) do
+      delete product_url(@product)
+    end
+
+  assert_redirected_to products_url
+  
 end
